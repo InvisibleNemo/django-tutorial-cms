bde1911f35579981445d1a80666307cb3d7f558d
InvisibleNemo <admin@invisiblenemo.com>
2023-01-18 12:23:37 -0500

    v0.11a - Added password reset

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -21,7 +21,14 @@ from djcrm.settings import STATIC_ROOT, STATIC_URL
 from leads.views import LandingPageView, SignUpView
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import (
+    LoginView, 
+    LogoutView, 
+    PasswordResetView, 
+    PasswordResetDoneView, 
+    PasswordResetConfirmView,
+    PasswordResetCompleteView
+    )
 
 
 urlpatterns = [
@@ -32,6 +39,11 @@ urlpatterns = [
     path('login/', LoginView.as_view(),name="login"),
     path('logout/', LogoutView.as_view(),name="logout"),
     path('signup/', SignUpView.as_view(),name="signup"),
+    path('reset-password/', PasswordResetView.as_view(), name='reset-password'),
+    path('password-reset-done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password-reset-complete/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
     
 ]
 
